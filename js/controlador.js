33c45677363a91f2b8fdc3e4a9237bb70d191a7f
Andres Vargas Ramirez <andreselias.vargas@gmail.com>
2019-06-06 14:46:16 -0600

    feature
    Structure HTML
    New Validate JS

--- js/controlador.js
+++ js/controlador.js
@@ -12,8 +12,9 @@ const btnAgregar = document.querySelector('#btnAgregar');
 
 //Regular Expressions
 const regexSimbolos = /[-!$%^&*()_+|~=`{}\[\]:";'<>?,.\/\\]/;
-const regexSimbolosNumeros = /[-!$%^&*()_+|~=`{}\[\]:";'<>?,.\/\\0-9]/;
 const regexEmail = /^(([^<>()[\]\.,;:\s@\"ñ#¿¡@´]+(\.[^<>()[\]\.,;:\s@\"ñ#¿¡@´]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"ñ#¿¡@´]+\.)+[^<>()[\]\.,;:\s@\"ñ#¿¡@´]{2,})$/i;
+const regexLetras = /^[A-Za-zäÄëËïÏöÖüÜáéíóúáéíóúÁÉÍÓÚÂÊÎÔÛâêîôûàèìòùÀÈÌÒÙ]+$/;
+const regexNumeros = /^([0-9])/;
 
 //Objeto Cliente
 const aCliente = [];
@@ -26,7 +27,6 @@ const validarCampos = () => {
     // let sEmail = inputEmail.value;
     // let nFondos = Number(inputFondos.value);
     // let nMensualidad = Number(inputMensualidad.value);
-    let validarEstado = true;
 
     let sNombre = 'Andrés';
     let sApellido = 'Vargas';
@@ -34,47 +34,99 @@ const validarCampos = () => {
     let nFondos = 10;
     let nMensualidad = 10;
 
+    let validarEstado = true;
+    let bError = validar(sNombre, sApellido, sEmail, nFondos, nMensualidad);
+
     //Validadores
-    if (nFondos === 0 || nMensualidad === 0) {
-        Swal.fire({
-            type: 'error',
-            title: 'Oops...',
-            text: 'El campo no puede estar vacio o ser igual a 0',
-        })
-        validarEstado = false;
-    } else if (sNombre === '' || sApellido === '' || sEmail === '' || nFondos === '' || nMensualidad === '') {
-        Swal.fire({
-            type: 'error',
-            title: 'Oops...',
-            text: 'Los campos no pueden estar vacios',
-        })
-        validarEstado = false;
-    } else if (regexSimbolosNumeros.test(sNombre) === true || regexSimbolosNumeros.test(sApellido) === true) {
-        Swal.fire({
-            type: 'error',
-            title: 'Oops...',
-            text: 'El campo no puede contener símbolos o números',
-        })
-        validarEstado = false;
-    } else if (regexSimbolos.test(nFondos) === true || regexSimbolos.test(nMensualidad) === true) {
-        Swal.fire({
-            type: 'error',
-            title: 'Oops...',
-            text: 'El campo no puede contener símbolos',
-        })
-        validarEstado = false;
-    } else if (regexEmail.test(sEmail) === false) {
-        Swal.fire({
-            type: 'error',
-            title: 'Oops...',
-            text: 'El email no es valido',
-        })
-        validarEstado = false;
+    if (bError == true) {
+        if (nFondos === 0 || nMensualidad === 0) {
+            Swal.fire({
+                type: 'error',
+                title: 'Oops...',
+                text: 'El campo no puede estar vacio o ser igual a 0',
+            })
+            validarEstado = false;
+        } else if (sNombre === '' || sApellido === '' || sEmail === '' || nFondos === '' || nMensualidad === '') {
+            Swal.fire({
+                type: 'error',
+                title: 'Oops...',
+                text: 'Los campos no pueden estar vacios',
+            })
+            validarEstado = false;
+        } else if (regexLetras.test(sNombre) === false || regexLetras.test(sApellido) === false) {
+            Swal.fire({
+                type: 'error',
+                title: 'Oops...',
+                text: 'El campo no puede contener símbolos o números',
+            })
+            validarEstado = false;
+        } else if (regexNumeros.test(nFondos) === false || regexNumeros.test(nMensualidad) === false) {
+            Swal.fire({
+                type: 'error',
+                title: 'Oops...',
+                text: 'El campo no puede contener símbolos',
+            })
+            validarEstado = false;
+        } else if (regexEmail.test(sEmail) === false) {
+            Swal.fire({
+                type: 'error',
+                title: 'Oops...',
+                text: 'El email no es valido',
+            })
+            validarEstado = false;
+        };
     };
 
 
     //AgregarDatos
     if (validarEstado) { agregarCliente(aCliente) };
 };
+
+//Valida los campos que se colocan en rojo
+const validar = (psNombre, psApellido, psEmail, pnFondos, pnMensualidad) => {
+    let bError = false;
+
+    //Valida Nombre
+    if (regexLetras.test(psNombre) == false || psNombre == '' || psNombre == null || psNombre == undefined) {
+        bError = true;
+        inputNombre.classList.add('border-danger');
+    } else {
+        inputNombre.classList.remove('border-danger');
+    };
+
+    //Valida Apellido
+    if (regexLetras.test(psApellido) == false || psApellido == '' || psApellido == null || psApellido == undefined) {
+        bError = true;
+        inputApellido.classList.add('border-danger');
+    } else {
+        inputApellido.classList.remove('border-danger');
+    };
+
+    //Valida Email
+    if (regexEmail.test(psEmail) == false || psEmail == '' || psEmail == null || psEmail == undefined) {
+        bError = true;
+        inputEmail.classList.add('border-danger');
+    } else {
+        inputEmail.classList.remove('border-danger');
+    };
+
+    //Valida Fondos
+    if (regexNumeros.test(pnFondos) == false || pnFondos == '' || pnFondos == null || pnFondos == undefined) {
+        bError = true;
+        inputFondos.classList.add('border-danger');
+    } else {
+        inputFondos.classList.remove('border-danger');
+    };
+
+    //Valida Mensualidad
+    if (regexNumeros.test(pnMensualidad) == false || pnMensualidad == '' || pnMensualidad == null || pnMensualidad == undefined) {
+        bError = true;
+        inputMensualidad.classList.add('border-danger');
+    } else {
+        inputMensualidad.classList.remove('border-danger');
+    };
+
+    return bError;
+}
 
 btnAgregar.addEventListener('click', validarCampos);
